b49d0e21286ff483104928101f682664bba9254e
Sergio Felzener <felzenerdj@gmail.com>
2020-10-13 16:19:11 -0300

    update

--- HardwareSpot/resources/views/admin/products/edit.blade.php
+++ HardwareSpot/resources/views/admin/products/edit.blade.php
@@ -67,9 +67,12 @@
 
     <div class="w-full pl-3 mt-3 ml-5 px-4 lg:w-1/2 xl:w-1/3">
         @foreach($product->photos as $photo)
-            <div class="flex   rounded-b-lg shadow-lg">
-                <img src="{{asset('storage/' . $photo->image)}}" alt="" class="w-32 flex-shrink-0 py-2 mr-2 ml-2">
+        <div class="flex rounded-b-lg shadow-lg">
+            <img src="{{asset('storage/' . $photo->image)}}" alt="" class="w-48 flex-shrink-0 py-2 mr-2 ml-2">
+            <div class= "mt-6">
+                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
             </div>
+        </div>
         @endforeach
     </div>
 
