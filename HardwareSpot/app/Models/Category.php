e94bc1913c52616426730455dcd9456d7e5b5e55
Sergio Felzener <felzenerdj@gmail.com>
2020-10-22 19:57:50 -0300

    finish Automatic Slugs

--- HardwareSpot/app/Models/Category.php
+++ HardwareSpot/app/Models/Category.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
     use HasFactory;
+    use HasSlug;
 
     protected $fillable = ['name','description', 'slug', 'img'];
+
+    public function getSlugOptions() : SlugOptions {
+
+        return SlugOptions::create()->generateSlugsFrom('name')->saveSlugsTo('slug');
+
+    }
 
     public function categories(){
         return $this->belongsToMany(Product::class);
